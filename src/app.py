11698027632ada0a47f84ed82e87b0d04a7ab86b
lovebird <lovebirdsx@gmail.com>
2023-04-15 12:59:08 +0800

    调整存储路径

--- src/app.py
+++ src/app.py
@@ -7,4 +7,4 @@ def get_save_path() -> str:
         home = os.getenv('USERPROFILE')
     if not home:
         raise Exception('无法获取用户主目录, 请检查环境变量是否设置HOME或USERPROFILE')
-    return os.path.normpath(os.path.join(home, '.chatgpt_tools'))
+    return os.path.normpath(os.path.join(home, '.chatgpt_tool'))
